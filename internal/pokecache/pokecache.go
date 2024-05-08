61f1a99274dd02be285e8d84c3ae3d44b35959d9
ArrayOfLilly <arrayoflilly@gmail.com>
2024-05-08 18:39:11 +0200

    caching

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -2,11 +2,12 @@ package pokecache
 
 import (
 	"time"
+  "sync"
 )
 
 type Cache struct {
 	cache map[string]cacheEntry
-	// mu sync.RWMutex
+	mu sync.RWMutex
 }
 
 type cacheEntry struct {
@@ -18,25 +19,25 @@ type cacheEntry struct {
 func NewCache(interval time.Duration) Cache {
 	c := Cache{
 		cache: make(map[string]cacheEntry),
-		// mu: sync.RWMutex{},
+		mu: sync.RWMutex{},
 	}
 	go c.reapLoop(interval)
 	return c
 }
 
 func (c *Cache) Add(key string, val []byte) {
-	// c.mu.Lock()
+	c.mu.Lock()
+  defer c.mu.Unlock()
 	c.cache[key] = cacheEntry{
 		createdAt: time.Now().UTC(),
 		val: val,
 	}
-	// c.mu.Unlock()
 }
 
 func (c *Cache) Get(key string) ([]byte, bool) {
-	// c.mu.RLock()
+	c.mu.RLock()
+  defer c.mu.RUnlock()
 	cachE, ok := c.cache[key]
-	// c.mu.RUnlock()
 	return cachE.val, ok
 }
 
@@ -51,9 +52,9 @@ func (c *Cache) reap(interval time.Duration) {
 	timeAgo := time.Now().UTC().Add(-interval)
 	for k, v := range c.cache {
 		if v.createdAt.Before(timeAgo) {
-			// c.mu.Lock()
+			c.mu.Lock()
+      defer c.mu.Unlock()
 			delete(c.cache, k)
-			// c.mu.Unock()
 		}
 	} 
 }
